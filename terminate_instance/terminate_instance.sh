6c9efc2063b647eefdaf9844fbf4a8ad6b13a3f6
vasanth889 <38485903+vasanth889@users.noreply.github.com>
2018-07-02 13:52:31 +0530

    Add files via upload

---
+++ terminate_instance/terminate_instance.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#  Copyright 2005-2012 WSO2, Inc. http://www.wso2.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+#os check
+if  cat /etc/*-release |grep 'NAME="Ubuntu"'
+then
+	echo "this is ubuntu"
+	#Terminate an instance
+
+	if pgrep -x "wso2server.sh" > /dev/null
+	then
+		echo "Running"
+		echo "KILL_application"
+		sleep 3
+		pkill -9 wso2server.sh
+	else
+		echo "Stopped"
+	
+	fi
+elif cat /etc/*-release | grep 'NAME="Red Hat Enterprise Linux Server"'
+then
+	echo "this is redhat"
+	if pgrep -x "wso2server.sh" > /dev/null
+	then
+		echo "Running"
+		echo "KILL_application"
+		sleep 3
+		pkill -9 wso2server.sh
+	else
+		echo "Stopped"
+	
+	fi
+elif cat /etc/*-release | grep 'NAME="CentOS Linux"'
+then
+	echo "this is centos"
+	if pgrep -x "wso2server.sh" > /dev/null
+	then
+		echo "Running"
+		echo "KILL_application"
+		sleep 3
+		pkill -9 wso2server.sh
+	else
+		echo "Stopped"
+	
+	fi
+else
+	echo "nothing matched"
+fi
